4533b297b337c96441e1c88ea80c7ed4f6e371f8
Jim Cook <jim.cook@sydney.edu.au>
2019-01-15 13:34:13 +1100

    added favicon, apercu font
    TODO:
    Main page
    
    Possibly:
    CSS Page Transitions?

--- js/main.js
+++ js/main.js
@@ -1,23 +1,14 @@
+function handleEntranceVideo() {
+    let heroBtn = $('#goto-main-btn'),
+        heroVid = $('#hero-video-container'),
+        heroText = $('#hero-text');
+    heroBtn.hide();
+    heroText.hide();
+    heroVid.on('loaded', function() {
+        heroBtn.fadeIn(2000);
+        heroText.fadeIn(2000);
+    });
+};
 $(document).ready(function() {
-    
-    // HERO ENTRANCE/LANDING: 360 Video.
-    // (function() {
-    //     var containerSelector = '#container360';
-    //     this.viewer = new Kaleidoscope.Video({
-    //         source: 'assets/vid/usyd360test.mp4',
-    //         containerId: containerSelector,
-    //         height: window.innerHeight,
-    //         width: window.innerWidth,
-    //         loop: true
-    //     });
-    //     this.viewer.render();
-    //     window.onresize = function() {
-    //         this.viewer.setSize({height: window.innerHeight, width: window.innerWidth});
-    //     }.bind(this);
-    //     document.querySelector(containerSelector).addEventListener('touchend', this.viewer.play.bind(this.viewer));
-    //     document.body.addEventListener('onclick', function() {
-    //         this.viewer.play();
-    //     }.bind(this));
-    // })();
-
+    handleEntranceVideo();
 });
