739ecad6c71f3590d45540b878b7550e93d2cf1b
Jim Cook <jim.cook@sydney.edu.au>
2019-02-12 09:52:16 +1100

    cleanup
    removed dev separate sections,
    
    now to implement scrolly scrolly :+1:

--- js/main.js
+++ js/main.js
@@ -11,21 +11,7 @@ function handleEntranceVideo() {
     });
 };
 
-function parallax() {
-    let scrolled = $(window).scrollTop();
-    $('.placeholder-banner').css('top', -(scrolled * 0.0315) + 'rem');
-    $('.placeholder-banner > h1').css('top', -(scrolled *= 0.005) + 'rem');
-    $('.placeholder-banner > h1').css('opacity', 1 - (scrolled * 0.3));
-}
-
 // main - DOM ready function.
 $(document).ready(function() {
-    skrollr.init();
-
     handleEntranceVideo();
-
-    $(window).scroll(function(e) {
-        parallax();
-    });
 });
-
