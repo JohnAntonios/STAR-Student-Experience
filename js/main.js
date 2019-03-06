173005c71c03cb901d291f852ee06887ade67bf3
Jim Cook <jim.cook@sydney.edu.au>
2019-03-06 15:52:48 +1100

    main complete

--- js/main.js
+++ js/main.js
@@ -5,13 +5,68 @@ function handleEntranceVideo() {
         heroText = $('#hero-text');
     heroBtn.hide();
     heroText.hide();
-    heroVid.on('loaded', function() {
+    heroVid.on('loaded', function () {
         heroBtn.fadeIn(2000);
         heroText.fadeIn(2000);
     });
 };
 
+function handleParallaxBanners() {
+    let controller = new ScrollMagic.Controller({
+        globalSceneOptions: { triggerHook: "onEnter", duration: "200%" }
+    });
+
+    new ScrollMagic.Scene({ triggerElement: "#parallax1" })
+        .setTween("#parallax1 > div", {
+            y: "80%",
+            ease: Linear.easeNone
+        })
+        .addIndicators()
+        .addTo(controller);
+
+    new ScrollMagic.Scene({ triggerElement: "#parallax2" })
+        .setTween("#parallax2 > div", {
+            y: "80%",
+            ease: Linear.easeNone
+        })
+        .addIndicators()
+        .addTo(controller);
+
+    new ScrollMagic.Scene({ triggerElement: "#parallax3" })
+        .setTween("#parallax3 > div", {
+            y: "80%",
+            ease: Linear.easeNone
+        })
+        .addIndicators()
+        .addTo(controller);
+
+    new ScrollMagic.Scene({ triggerElement: "#parallax4" })
+        .setTween("#parallax4 > div", {
+            y: "80%",
+            ease: Linear.easeNone
+        })
+        .addIndicators()
+        .addTo(controller);
+
+    new ScrollMagic.Scene({ triggerElement: "#parallax5" })
+        .setTween("#parallax5 > div", {
+            y: "80%",
+            ease: Linear.easeNone
+        })
+        .addIndicators()
+        .addTo(controller);
+
+    new ScrollMagic.Scene({ triggerElement: "#parallax6" })
+        .setTween("#parallax6 > div", {
+            y: "80%",
+            ease: Linear.easeNone
+        })
+        .addIndicators()
+        .addTo(controller);
+}
+
 // main - DOM ready function.
-$(document).ready(function() {
+$(document).ready(function () {
     handleEntranceVideo();
+    handleParallaxBanners();
 });
