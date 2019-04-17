214c92808e8601ab5217eafcaacdd0d7650ed4f0
Jim Cook <jim.cook@sydney.edu.au>
2019-04-18 08:38:35 +1000

    modularising initiate

--- js/main.js
+++ js/main.js
@@ -1,72 +1,36 @@
 // entrance index video load.
 function handleEntranceVideo() {
-    let heroBtn = $('#goto-main-btn'),
-        heroVid = $('#hero-video-container'),
-        heroText = $('#hero-text');
-    heroBtn.hide();
-    heroText.hide();
-    heroVid.on('loaded', function () {
-        heroBtn.fadeIn(2000);
-        heroText.fadeIn(2000);
-    });
-};
-
-function handleParallaxBanners() {
-    let controller = new ScrollMagic.Controller({
-        globalSceneOptions: { triggerHook: "onEnter", duration: "200%" }
-    });
-
-    new ScrollMagic.Scene({ triggerElement: "#parallax1" })
-        .setTween("#parallax1 > div", {
-            y: "80%",
-            ease: Linear.easeNone
-        })
-        .addIndicators()
-        .addTo(controller);
-
-    new ScrollMagic.Scene({ triggerElement: "#parallax2" })
-        .setTween("#parallax2 > div", {
-            y: "80%",
-            ease: Linear.easeNone
-        })
-        .addIndicators()
-        .addTo(controller);
-
-    new ScrollMagic.Scene({ triggerElement: "#parallax3" })
-        .setTween("#parallax3 > div", {
-            y: "80%",
-            ease: Linear.easeNone
-        })
-        .addIndicators()
-        .addTo(controller);
-
-    new ScrollMagic.Scene({ triggerElement: "#parallax4" })
-        .setTween("#parallax4 > div", {
-            y: "80%",
-            ease: Linear.easeNone
-        })
-        .addIndicators()
-        .addTo(controller);
-
-    new ScrollMagic.Scene({ triggerElement: "#parallax5" })
-        .setTween("#parallax5 > div", {
-            y: "80%",
-            ease: Linear.easeNone
-        })
-        .addIndicators()
-        .addTo(controller);
-
-    new ScrollMagic.Scene({ triggerElement: "#parallax6" })
-        .setTween("#parallax6 > div", {
-            y: "80%",
-            ease: Linear.easeNone
-        })
-        .addIndicators()
-        .addTo(controller);
+	let heroBtn = $("#goto-main-btn"),
+		heroVid = $("#hero-video-container"),
+		heroText = $("#hero-text");
+	heroBtn.hide();
+	heroText.hide();
+	heroVid.on("loaded", function() {
+		heroBtn.fadeIn(2000);
+		heroText.fadeIn(2000);
+	});
 }
 
+// progress bar on bottom of page
+function updateProgress(num1, num2) {
+	var percent = Math.ceil((num1 / num2) * 100);
+	var progressBar = document.getElementById("progress");
+	progressBar.style.width = percent + "%";
+}
+
+window.addEventListener("scroll", function() {
+	var top = window.scrollY;
+	var height =
+		document.body.getBoundingClientRect().height - window.innerHeight;
+	updateProgress(top, height);
+});
+
+function handlePagination() {
+	let restDiv = document.querySelector("div#rest");
+	restDiv.style.display = "none"
+}
 // main - DOM ready function.
-$(document).ready(function () {
-    handleEntranceVideo();
-    handleParallaxBanners();
+$(document).ready(function() {
+	handleEntranceVideo();
+	handlePagination();
 });
