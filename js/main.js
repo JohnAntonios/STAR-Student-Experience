66c4af465539818336bc97a00053ad9cf40472e5
John Antonios <john.antonios@sydney.edu.au>
2019-09-27 08:46:15 +1000

    embedded all videos

--- js/main.js
+++ js/main.js
@@ -1,72 +1,17 @@
 // entrance index video load.
 function handleEntranceVideo() {
-    let heroBtn = $('#goto-main-btn'),
-        heroVid = $('#hero-video-container'),
-        heroText = $('#hero-text');
-    heroBtn.hide();
-    heroText.hide();
-    heroVid.on('loaded', function () {
-        heroBtn.fadeIn(2000);
-        heroText.fadeIn(2000);
-    });
-};
-
-function handleParallaxBanners() {
-    let controller = new ScrollMagic.Controller({
-        globalSceneOptions: { triggerHook: "onEnter", duration: "200%" }
-    });
-
-    new ScrollMagic.Scene({ triggerElement: "#parallax1" })
-        .setTween("#parallax1 > div", {
-            y: "80%",
-            ease: Linear.easeNone
-        })
-        .addIndicators()
-        .addTo(controller);
-
-    new ScrollMagic.Scene({ triggerElement: "#parallax2" })
-        .setTween("#parallax2 > div", {
-            y: "80%",
-            ease: Linear.easeNone
-        })
-        .addIndicators()
-        .addTo(controller);
-
-    new ScrollMagic.Scene({ triggerElement: "#parallax3" })
-        .setTween("#parallax3 > div", {
-            y: "80%",
-            ease: Linear.easeNone
-        })
-        .addIndicators()
-        .addTo(controller);
-
-    new ScrollMagic.Scene({ triggerElement: "#parallax4" })
-        .setTween("#parallax4 > div", {
-            y: "80%",
-            ease: Linear.easeNone
-        })
-        .addIndicators()
-        .addTo(controller);
-
-    new ScrollMagic.Scene({ triggerElement: "#parallax5" })
-        .setTween("#parallax5 > div", {
-            y: "80%",
-            ease: Linear.easeNone
-        })
-        .addIndicators()
-        .addTo(controller);
-
-    new ScrollMagic.Scene({ triggerElement: "#parallax6" })
-        .setTween("#parallax6 > div", {
-            y: "80%",
-            ease: Linear.easeNone
-        })
-        .addIndicators()
-        .addTo(controller);
+  let heroBtn = $("#goto-main-btn"),
+    heroVid = $("#hero-video-container"),
+    heroText = $("#hero-text");
+  heroBtn.hide();
+  heroText.hide();
+  heroVid.on("loaded", function() {
+    heroBtn.fadeIn(2000);
+    heroText.fadeIn(2000);
+  });
 }
 
 // main - DOM ready function.
-$(document).ready(function () {
-    handleEntranceVideo();
-    handleParallaxBanners();
+$(document).ready(function() {
+  handleEntranceVideo();
 });
